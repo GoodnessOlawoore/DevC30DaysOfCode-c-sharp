a7991ee3c60262de459e8da812bb50331fa317c1
olawoore <olawooregoodness6@gmail.com>
2020-04-26 04:50:02 +0100

    Added code for getting data type

--- writing-console-apps/2-data-types/Program.cs
+++ writing-console-apps/2-data-types/Program.cs
@@ -31,6 +31,26 @@ namespace _2_data_types
                 Console.WriteLine("He is truely Handsome");
             else
                 Console.WriteLine("He is not Handsome");
+
+            //Getting Data Types 
+            //For integer
+            int Number = 22;
+            Console.WriteLine($"{Number} is of data type {Number.GetType()}");
+            //For long
+            long PhoneNumber = 2348199887766;
+            Console.WriteLine($"{PhoneNumber} is of data type {PhoneNumber.GetType()}");   
+            //For double
+            double FirstDouble = 10.6;
+            Console.WriteLine($"{FirstDouble} is of data type {FirstDouble.GetType()}");
+            //For float
+            float FloatNumber = 5.8f;
+            Console.WriteLine($"{FloatNumber} is of data type {FloatNumber.GetType()}");
+            //For character
+            char AnotherChar = 'A'; //Note that  a single quote is used in char unlike string that uses double quote
+            Console.WriteLine($"{AnotherChar} is of data type {AnotherChar.GetType()}");
+            //For strings
+            string Challenge = "DevC30DaysOfCode Challenge";
+            Console.WriteLine($"{Challenge} is of data type {Challenge.GetType()}"); 
         }
     }
 }
