ae8994628bb4341a1d949a0603b2c6b032ded539
olawoore <olawooregoodness6@gmail.com>
2020-04-26 05:24:50 +0100

    commit

--- writing-console-apps/5-operators/Program.cs
+++ writing-console-apps/5-operators/Program.cs
@@ -6,7 +6,32 @@ namespace _5_operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            *Arithmetic Operators
+            *Logical Operators
+            */
+
+            //ARITHMETIC OPERATORS. 
+            //Addition
+            int FirstNumber = 10;
+            int SecondNumber = 4;
+            Console.WriteLine(FirstNumber + SecondNumber);
+            //Subtraction
+            int NewNumber = 10;
+            int NewNumber2 = 4;
+            Console.WriteLine(NewNumber - NewNumber2);
+            //Multiplication
+            int TakeNumber = 10;
+            int TakeNumber2 = 4;
+            Console.WriteLine(TakeNumber * TakeNumber2);
+            //Division
+            int DivisionNumber = 10;
+            int DivisionNumber2 = 4;
+            Console.WriteLine(DivisionNumber / DivisionNumber2);
+            //Remainder 
+            int NewNum = 10;
+            int NewNum2 = 4;
+            Console.WriteLine(NewNumber % NewNumber2);
         }
     }
 }
