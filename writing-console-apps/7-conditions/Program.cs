bd1a46b44135072b4e52049ba90eceb125f6ee3c
olawoore <olawooregoodness6@gmail.com>
2020-04-30 13:55:25 +0100

    commit

--- writing-console-apps/7-conditions/Program.cs
+++ writing-console-apps/7-conditions/Program.cs
@@ -5,8 +5,23 @@ namespace _7_conditions
     class Program
     {
         static void Main(string[] args)
-        {
-            //>>>>>>>>>>>>>>>
+        {   
+            //Simple if statement with integer
+            int a = 5;
+            int b = 4;
+            if (a > b)
+            {
+                Console.WriteLine("A is greater than B");
+            } 
+            else
+            {
+                Console.WriteLine("A is less than or equal to B");
+            }
+
+            
+            
+            
+
         }
     }
 }
