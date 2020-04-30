383a232edad9d4cf67c64d5b85670e729eb8bc60
olawoore <olawooregoodness6@gmail.com>
2020-04-30 09:43:33 +0100

    commit

--- writing-console-apps/6-accepting-input/Program.cs
+++ writing-console-apps/6-accepting-input/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace _6_accepting_input
+namespace _4_string_interpolation_and_concatenation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Fix this tomorrow!
+            int Number;
+            Console.WriteLine("Enter a number:  ");
+            //int.Parse converts the string to integer 
+            Number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You have entered: {Number}");
+            Console.ReadLine();
+
+            string Text;
+            Console.WriteLine("enter some text here:  ");
+            Text = Console.ReadLine();
+            Console.WriteLine($"Your input was: {Text}");
+            Console.ReadLine();
+            /*these lines of code below performs thesame function as the line of code above
+            *Console.WriteLine("your input was: {0}", StringInput);
+            *Console.WriteLine("your input was: " + StringInput);
+            */
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a decimal number: ");
+            double DoubleInput = double.Parse(Console.ReadLine());
+            Console.Write($"Your input was: {DoubleInput}");
+            Console.ReadLine();
         }
     }
 }
