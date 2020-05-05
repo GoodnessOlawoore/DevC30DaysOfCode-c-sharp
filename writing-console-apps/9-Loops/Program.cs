7208308e14f9d3e05474dd1e036092a019a8b18d
olawoore <olawooregoodness6@gmail.com>
2020-05-05 11:05:51 +0100

    commit

--- writing-console-apps/9-Loops/Program.cs
+++ writing-console-apps/9-Loops/Program.cs
@@ -7,6 +7,30 @@ namespace _9_Loops
         static void Main(string[] args)
         {
             //Loops
+            
+            //FOR LOOP
+            //   initialisation ; condition  ; step
+            for (int Numbers = 0; Numbers < 5; Numbers++)
+            {
+                Console.WriteLine(Numbers);
+            }
+
+            //OR
+            int Number = 0;
+            for(;;)
+            {
+                if(Number < 5)
+                {
+                    Console.WriteLine(Number); Number++;
+                }
+                else break;
+            }
+
+            // For Steps = Subtraction
+            for ( int Num = 5; Num > 0; Num--)
+            {
+                Console.WriteLine(Num);
+            }
         }
     }
 }
